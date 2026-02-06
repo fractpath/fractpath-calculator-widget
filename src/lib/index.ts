b2e6ca20a44c2f6de7ac00e6a8ba402f23568f6b
Alex <admin@fractpath.com>
2026-02-06 13:49:17 +0000

    feat(widget): add library export surface

---
+++ src/lib/index.ts
@@ -0,0 +1,8 @@
+export { FractPathCalculatorWidget } from "../widget/FractPathCalculatorWidget";
+export type {
+  CalculatorPersona,
+  CalculatorMode,
+  LeadPayload,
+  WidgetEvent,
+  FractPathCalculatorWidgetProps,
+} from "../widget/types";
