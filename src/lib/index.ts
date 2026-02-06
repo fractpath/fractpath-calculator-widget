c529386c5189e3f534d408e4dc3eba65b3d887f3
Alex <admin@fractpath.com>
2026-02-06 14:04:48 +0000

    feat(chart): add buildChartSeries and export calc API

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,4 +1,8 @@
 export { FractPathCalculatorWidget } from "../widget/FractPathCalculatorWidget.js";
+
+export { computeScenario, normalizeInputs } from "../calc/calc.js";
+export { buildChartSeries } from "../calc/chart.js";
+
 export type {
   CalculatorPersona,
   CalculatorMode,
@@ -6,3 +10,17 @@ export type {
   WidgetEvent,
   FractPathCalculatorWidgetProps,
 } from "../widget/types.js";
+
+export type {
+  ScenarioInputs,
+  ScenarioOutputs,
+  SettlementResult,
+  SettlementTiming,
+  TimePoint,
+} from "../calc/types.js";
+
+export type {
+  ChartPoint,
+  SettlementMarker,
+  ChartSeries,
+} from "../calc/chart.js";
